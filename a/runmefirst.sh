a73dfbf32b1181c25c07a0302cc09269b718f274
danl429 <137623523+danl429@users.noreply.github.com>
2024-11-02 19:45:52 +0000

    j

--- a/runmefirst.sh
+++ a/runmefirst.sh
@@ -1,9 +1,25 @@
 sudo apt-get update
-sudo apt-get -y install tmux
-sudo apt-get -y install emacs
+#sudo apt-get -y install tmux
+#sudo apt-get -y install elinks
+#sudo apt-get -y install w3m
+sudo apt-get -y install urlview
+#sudo apt-get -y install emacs
+#
+#cat >> $HOME/.bashrc <<NICE
+#export PS1='% '
+#NICE
+#
+#source $HOME/.bashrc
 
-cat >> $HOME/.bashrc <<NICE
-export PS1='% '
-NICE
+mkdir -p ~/.mutt
+cp /etc/Muttrc.d/colors.rc ~/.mutt
+cp /etc/mailcap ~/.mailcap
 
-source $HOME/.bashrc
+
+#cat >> $HOME/.muttrc <<EOF
+#auto_view text/html
+#alternative_order text/plain text text/enriched text/html
+#EOF
+
+
+git clone https://github.com/stolk/imcat.git # CLI image viewer
